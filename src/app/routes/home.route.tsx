c64ab65022ce9fc46c85bf4b3b722485d6d30840
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-15 10:40:09 +0900

    chore: 레이아웃 및 라우팅 초기 설정

--- src/app/routes/home.route.tsx
+++ src/app/routes/home.route.tsx
@@ -1,7 +1,16 @@
 import type { RouteObject } from "react-router-dom";
 import HomePage from "@pages/home/ui/home.page";
+import BasicLayout from "../layouts/basic.layout";
+import ErrorLayout from "../layouts/error.layout";
 
 export const HomeRoute: RouteObject = {
   path: "/",
-  element: <HomePage />,
+  element: <BasicLayout />,
+  children: [
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ],
+  errorElement: <ErrorLayout />,
 };
