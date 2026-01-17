f7502cf68fd40993776e8c9ae91f87c6f4bdf1c5
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-18 02:43:37 +0900

    feat : 즐겨찾기 성공/실패 토스트 알림 기능

--- src/app/layouts/basic.layout.tsx
+++ src/app/layouts/basic.layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
+import { Toaster } from "@/shared/ui/_shadcn/sonner";
 
 function BasicLayout() {
   return (
-    <div className="flex flex-col items-center w-screen h-screen ">
+    <div className="flex flex-col items-center w-screen h-screen">
       <Header />
       <Outlet />
+      <Toaster />
     </div>
   );
 }
--- src/features/favorite/ui/favorite-toggle-button.tsx
+++ src/features/favorite/ui/favorite-toggle-button.tsx
@@ -1,4 +1,5 @@
 import { useFavoritesStore } from "@/shared/store/favorites.store";
+import { toast } from "sonner";
 
 function FavoriteToggleButton({ district }: { district: string }) {
   const { favorites, addFavorite, removeFavorite } = useFavoritesStore();
@@ -6,8 +7,10 @@ function FavoriteToggleButton({ district }: { district: string }) {
   const handleFavoriteClick = (district: string) => {
     if (favorites.some((favorite) => favorite.district === district)) {
       removeFavorite(district);
+      toast.error(`"${district.split("-").slice(-2).join(" ")}"이(가) 관심지역에서 삭제되었습니다.`);
     } else {
       addFavorite({ district });
+      toast.success(`"${district.split("-").slice(-2).join(" ")}"이(가) 관심지역으로 추가되었습니다.`);
     }
   };
   return (
