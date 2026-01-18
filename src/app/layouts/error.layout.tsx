e96690233c32812997a24ecb8b0e28dc170c8411
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-18 19:21:55 +0900

    chore: add gitignore and error layout

--- src/app/layouts/error.layout.tsx
+++ src/app/layouts/error.layout.tsx
@@ -1,5 +1,5 @@
 function ErrorLayout() {
-  return <div>ErrorLayout</div>;
+    return <div className="flex flex-col justify-center items-center w-screen h-screen"> 잘못된 접근입니다.</div>;
 }
 
 export default ErrorLayout;
