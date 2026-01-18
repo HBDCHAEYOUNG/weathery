a1717d4da1b9bac9d342619385731f095cec0235
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-18 19:05:04 +0900

    feat: 관심지역 삭제 기능

--- src/features/favorite/ui/favorite-item.tsx
+++ src/features/favorite/ui/favorite-item.tsx
@@ -1,56 +1,62 @@
 import { encodeDistrict } from "@/shared/lib/url-utils";
 import useWeatherData from "@/features/weather/model/use-weather-data";
 import { Link } from "react-router-dom";
+import CircleButton from "@/shared/ui/button/circle-button";
+import { useFavoritesStore } from "@/shared/store/favorites.store";
 
-//TODO: 즐겨찾기 삭제 기능 추가
-function FavoriteItem({ district }: { district: string }) {
+function FavoriteItem({ district, editMode }: { district: string, editMode: boolean }) {
   const { nowcastData, forecastData } = useWeatherData({ 
     district, 
     numOfRows: 200 
   });
+  const { removeFavorite } = useFavoritesStore();
 
   const partsDistrict = district.split("-");
   
+  const handleDelete = () => {
+    removeFavorite(district);
+  };
+  
   return (
-    <Link
-      to={`/${encodeDistrict(district)}`}
-      className="flex gap-2 justify-between items-center border border-gray-300 p-4 rounded-md relative hover:shadow-md"
-    >
-      {/* <button
-        className="absolute top-2 right-4 bg-transparent text-xl cursor-pointer hover:text-red-500"
-        onClick={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          removeFavorite(district);
-        }}
+    <div className="relative">
+      <Link
+        to={`/${encodeDistrict(district)}`}
+        className="flex gap-2 justify-between items-center border border-gray-300 p-4 rounded-md relative hover:shadow-md"
       >
-        ✕
-      </button> */}
-      <div className="flex flex-col justify-between h-full">
-        <h1 className="font-semibold text-lg! mb-auto">
-          {partsDistrict.slice(-1)}
-        </h1>
-        <small className="text-gray-500 font-semibold">
-          대한민국 {partsDistrict.slice(0, -1).join(" ")}
-        </small>
-      </div>
-
-      <div className="flex flex-col items-center gap-2 md:flex-row">
-        <div className="flex items-center gap-2 md:mr-40">
-          <span className="text-4xl">{forecastData?.weatherIcon || ""}</span>
-          <p className="text-2xl font-bold">
-            {nowcastData?.currentTemp
-              ? `${Math.round(nowcastData.currentTemp)}°`
-              : "-"}
-          </p>
+        <div className="flex flex-col justify-between h-full">
+          <h1 className="font-semibold text-lg! mb-auto">
+            {partsDistrict.slice(-1)}
+          </h1>
+          <small className="text-gray-500 font-semibold">
+            대한민국 {partsDistrict.slice(0, -1).join(" ")}
+          </small>
         </div>
 
-        <small className="text-gray-500 font-semibold md:text-xl">
-          최고 {forecastData?.maxTemp ? `${Math.round(forecastData.maxTemp)}°` : "-"} / 최저{" "}
-          {forecastData?.minTemp ? `${Math.round(forecastData.minTemp)}°` : "-"}
-        </small>
-      </div>
-    </Link>
+        <div className="flex flex-col items-center gap-2 md:flex-row">
+          <div className="flex items-center gap-2 md:mr-40">
+            <span className="text-4xl">{forecastData?.weatherIcon || ""}</span>
+            <p className="text-2xl font-bold">
+              {nowcastData?.currentTemp
+                ? `${Math.round(nowcastData.currentTemp)}°`
+                : "-"}
+            </p>
+          </div>
+
+          <small className="text-gray-500 font-semibold md:text-xl">
+            최고 {forecastData?.maxTemp ? `${Math.round(forecastData.maxTemp)}°` : "-"} / 최저{" "}
+            {forecastData?.minTemp ? `${Math.round(forecastData.minTemp)}°` : "-"}
+          </small>
+        </div>
+      </Link>
+      
+      {editMode && (
+        <div className="group absolute inset-0 bg-black/20 rounded-md z-10 flex items-center justify-center hover:shadow-md">
+          <CircleButton onClick={handleDelete} className="z-20 cursor-pointer group-hover:scale-110 transition-all duration-300">
+            ✕
+          </CircleButton>
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/pages/favorites/ui/favorites.page.tsx
+++ src/pages/favorites/ui/favorites.page.tsx
@@ -1,15 +1,21 @@
 import FavoriteItem from "@/features/favorite/ui/favorite-item";
 import { useDropdownStore } from "@/shared/store/dropdown.store";
 import { useFavoritesStore } from "@/shared/store/favorites.store";
+import { useState } from "react";
 
 function FavoritesPage() {
   const { favorites } = useFavoritesStore();
   const { toggle } = useDropdownStore();
 
+  const [editMode, setEditMode] = useState(false);
+
   return (
     <div className=" grid grid-cols-1 gap-4 w-full common-padding-x common-padding-bottom max-w">
+      <button onClick={() => setEditMode(!editMode)} className="cursor-pointer ml-auto">
+        {editMode ? "완료" : "편집하기"}
+      </button>
       {favorites.map((favorite) => (
-        <FavoriteItem key={favorite.district} district={favorite.district} />
+        <FavoriteItem key={favorite.district} district={favorite.district} editMode={editMode} />
       ))}
       {favorites.length < 6 && <button onClick={toggle} className="cursor-pointer mb-10">🔍 도시 추가하기</button>}
     </div>
